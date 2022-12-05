27cade9f328a42417e4015c5df0efc7dbcb1dbcf
Tomas-caula <70955134+Tomas-caula@users.noreply.github.com>
2022-12-05 13:27:18 -0300

    as

---
+++ portafolio/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  return (
+<body> 
+  <div className='introduccion'> 
+    <h1>TOMAS CAULA</h1>
+    <h2>Software Developer</h2>
+  </div>
+  <div>
+    <h1>Mis Proyecto </h1>
+  </div>
+</body>
+    
+  );
+}
+
+export default App;
