30c261e259c1d288adaef07665bce6c20f603a7d
Tomas Benjamin  Caula <tomasbenjamincaula@gmail.com>
2024-12-20 01:06:24 -0300

    Changes in portFoliio

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,11 +1,14 @@
 import emailjs from '@emailjs/browser';
 import { useState } from 'react';
 import styles from './ContactForm.module.css';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Contact = () => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [message, setMessage] = useState('');
+  const [statusMessage, setStatusMessage] = useState('');
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -25,39 +28,45 @@ const Contact = () => {
       .send(serviceId, templateId, templateParams, publicKey)
       .then((response) => {
         console.log('email sent successfully', response);
+        toast.success('Message sent successfully!');
         setName('');
         setEmail('');
         setMessage('');
       })
       .catch((error) => {
         console.log('error sending email', error);
+        toast.error('Failed to send message. Please try again.');
       });
   };
 
   return (
-    <form onSubmit={handleSubmit} className={styles.form}>
-      <input
-        type='text'
-        placeholder='Your Name'
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-        className={styles.input}
-      />
-      <input
-        type='email'
-        placeholder='Your Email'
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        className={styles.input}
-      />
-      <textarea
-        placeholder='Your Message'
-        value={message}
-        onChange={(e) => setMessage(e.target.value)}
-        className={styles.textarea}
-      />
-      <input type='submit' value='Send Email' className={styles.button} />
-    </form>
+    <div className={styles.contactContainer}>
+      <ToastContainer position="top-right" />
+      <form onSubmit={handleSubmit} className={`${styles.form} ${styles.largeForm}`}>
+        <h2 className={styles.formTitle}>Contact Us</h2>
+        <input
+          type='text'
+          placeholder='Your Name'
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          className={`${styles.input} ${styles.largeInput}`}
+        />
+        <input
+          type='email'
+          placeholder='Your Email'
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className={`${styles.input} ${styles.largeInput}`}
+        />
+        <textarea
+          placeholder='Your Message'
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          className={`${styles.textarea} ${styles.largeTextarea}`}
+        />
+        <input type='submit' value='Send Email' className={`${styles.button} ${styles.largeButton}`} />
+      </form>
+    </div>
   );
 };
 
